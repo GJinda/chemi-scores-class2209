1a25324158105a8d04d616e2fd310d89b026c77c
takaidada <takaidada@163.com>
2022-03-28 12:15:46 +0800

    finish scores display in chart (class 2209 dongfeng IGS until 220318

---
+++ data.js
@@ -0,0 +1,178 @@
+const class9 = [
+  {
+    name: "zengfuqiang",
+    zname: "曾富强"
+  },
+  {
+    name: "zhangzixuan",
+    zname: "张梓萱"
+  },
+  {
+    name: "wanchaojun",
+    zname: "万超君"
+  },
+  {
+    name: "liuhanwen",
+    zname: "刘瀚文"
+  },
+  {
+    name: "zenghuazhe",
+    zname: "曾华喆"
+  },
+  {
+    name: "gengziming",
+    zname: "耿梓铭"
+  },
+  {
+    name: "baoxuesha",
+    zname: "鲍雪莎"
+  },
+  {
+    name: "qiuyubo",
+    zname: "邱宇博"
+  },
+  {
+    name: "zhangyuze",
+    zname: "张钰泽"
+  },
+  {
+    name: "liuyani",
+    zname: "刘亚妮"
+  },
+  {
+    name: "tangruibo",
+    zname: "唐瑞博"
+  },
+  {
+    name: "xuezihan",
+    zname: "薛梓涵"
+  },
+  {
+    name: "wuqingrui",
+    zname: "吴卿瑞"
+  },
+  {
+    name: "wangyifan",
+    zname: "汪一钒"
+  },
+  {
+    name: "chenyuwen",
+    zname: "陈昱文"
+  },
+  {
+    name: "lijiaqi",
+    zname: "李嘉奇"
+  },
+  {
+    name: "zhangxinyao",
+    zname: "张焮尧"
+  },
+  {
+    name: "liujingyao",
+    zname: "刘景尧"
+  },
+  {
+    name: "mofan",
+    zname: "莫凡"
+  },
+  {
+    name: "wuwenxin",
+    zname: "吴文欣"
+  },
+  {
+    name: "pangxianbing",
+    zname: "庞贤秉"
+  },
+  {
+    name: "chenfuhao",
+    zname: "陈富豪"
+  },
+  {
+    name: "lichenyang",
+    zname: "李晨阳"
+  },
+  {
+    name: "lilixuan",
+    zname: "李力轩"
+  },
+  {
+    name: "chaixinyu",
+    zname: "柴鑫雨"
+  },
+  {
+    name: "liuruisi",
+    zname: "刘睿斯"
+  },
+  {
+    name: "zhaozihan",
+    zname: "赵子涵"
+  },
+  {
+    name: "zhangwenwen",
+    zname: "张雯雯"
+  },
+  {
+    name: "wangyuchen",
+    zname: "王雨晨"
+  },
+  {
+    name: "wangwenna",
+    zname: "王文娜"
+  },
+  {
+    name: "wangmingjie",
+    zname: "王明杰"
+  },
+  {
+    name: "zhousiyuan",
+    zname: "周思源"
+  },
+  {
+    name: "yexiaoying",
+    zname: "叶晓颖"
+  },
+  {
+    name: "yanhao",
+    zname: "晏昊"
+  },
+  {
+    name: "quxinyi",
+    zname: "瞿欣怡"
+  },
+  {
+    name: "zanyanlin",
+    zname: "昝琰霖"
+  },
+  {
+    name: "tianjiaxin",
+    zname: "田嘉鑫"
+  },
+  {
+    name: "liujunjie",
+    zname: "刘俊杰"
+  },
+  {
+    name: "zhujinyi",
+    zname: "朱静祎"
+  },
+  {
+    name: "dangtiantian",
+    zname: "党田田"
+  },
+  {
+    name: "wangyixuan",
+    zname: "汪奕璇"
+  },
+  {
+    name: "yukun",
+    zname: "余坤"
+  },
+  {
+    name: "wuyijie",
+    zname: "吴怡洁"
+  },
+  {
+    name: "chenyuzhen",
+    zname: "陈昱臻"
+  }
+]
---
+++ script.js
@@ -0,0 +1,186 @@
+
+function getMaxLength(arr) {
+  //console.log(arr);
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > max) {
+      max = arr[i].length;
+    }
+  }
+  return max;
+}
+
+let chartDom1 = document.getElementById('table1');
+let chartDom2 = document.getElementById('table2');
+let chartDom3 = document.getElementById('table3');
+let chartDom4 = document.getElementById('table4');
+
+let lineChart1 = echarts.init(chartDom1);
+let lineChart2 = echarts.init(chartDom2);
+let lineChart3 = echarts.init(chartDom3);
+let lineChart4 = echarts.init(chartDom4);
+
+let app = {};
+
+let option1 = {
+  legend: {},
+  tooltip: {},
+  dataset: {
+    source: []
+  },
+  // x: category (first line of dataset)
+  xAxis: { type: 'category' },
+  // y: value。
+  yAxis: {},
+  // multiple bars (each line of dataset)
+  series: []
+}
+
+let option2 = {
+  legend: {},
+  tooltip: {},
+  dataset: {
+    source: []
+  },
+  // x: category (first line of dataset)
+  xAxis: { type: 'category' },
+  // y: value。
+  yAxis: {},
+  // multiple bars (each line of dataset)
+  series: []
+}
+
+let option3 = {
+  legend: {},
+  tooltip: {},
+  dataset: {
+    source: []
+  },
+  // x: category (first line of dataset)
+  xAxis: { type: 'category' },
+  // y: value。
+  yAxis: {},
+  // multiple bars (each line of dataset)
+  series: []
+}
+
+let option4 = {
+  legend: {},
+  tooltip: {},
+  dataset: {
+    source: []
+  },
+  // x: category (first line of dataset)
+  xAxis: { type: 'category' },
+  // y: value。
+  yAxis: {},
+  // multiple bars (each line of dataset)
+  series: []
+}
+
+let sheet = {}
+
+const readFile = (obj) => {
+  let file = obj.files[0];
+  let reader = new FileReader();
+  reader.readAsBinaryString(file);
+
+  reader.onload = function (e) {
+    let data = e.target.result;
+    let workbook = XLSX.read(data, { type: 'binary' });
+    sheet = workbook.Sheets[workbook.SheetNames[0]]
+    // console.log(sheet)
+    setData(sheet)
+
+  };
+}
+
+const setSource = (sheet, source, categoryNum, barsi, barsj) => {
+  let keys = Object.keys(sheet)
+  console.log("keys", keys, "last key", keys[keys.length - 2])
+
+  for (let i = 0; i < categoryNum; i++) {
+    let chari = String.fromCharCode(65 + i)
+    let arr = []
+    arr.push(sheet[chari + "1"].v)
+    for (let j = barsi; j < barsj; j++) {
+      arr.push(sheet[chari + (j + 1)].v)
+      // console.log("arr", arr)
+      // console.log("i", i)
+      // console.log("j", j)
+      // console.log("chari + (j + 1)", chari + (j + 1))
+      // console.log("sheet[chari + (j + 1)].v", sheet[chari + (j + 1)].v)
+      // console.log("keys[keys.length - 2]", keys[keys.length - 2])
+    }
+    source.push(arr)
+  }
+}
+
+const setSeries = (series, seriesNum, seriesType) => {
+  series.splice(0, series.length);
+  for (let i = 0; i < seriesNum; i++) {
+    let obj = { type: seriesType, smooth: "true" }
+    series.push(obj)
+  }
+}
+
+const setData = (sheet) => {
+  let keys = Object.keys(sheet)
+  console.log("keys", keys, "last key", keys[keys.length - 2])
+  let categoryNum = parseInt(keys.length / 45)
+  console.log("categoryNum", categoryNum)
+
+  let source1 = []
+  setSource(sheet, source1, categoryNum + 1, 1, 11)
+  let source2 = []
+  setSource(sheet, source2, categoryNum + 1, 11, 23)
+  let source3 = []
+  setSource(sheet, source3, categoryNum + 1, 23, 34)
+  let source4 = []
+  setSource(sheet, source4, categoryNum + 1, 34, 44)
+
+  console.log("source1", source1)
+  option1.dataset.source = source1;
+  console.log("source2", source2)
+  option2.dataset.source = source2;
+  console.log("source3", source3)
+  option3.dataset.source = source3;
+  console.log("source4", source4)
+  option4.dataset.source = source4;
+
+  let series1 = []
+  setSeries(series1, 11, 'line')
+  let series2 = []
+  setSeries(series2, 11, 'line')
+  let series3 = []
+  setSeries(series3, 11, 'line')
+  let series4 = []
+  setSeries(series4, 10, 'line')
+
+  console.log("series1", series1)
+  option1.series = series1;
+  console.log("series2", series2)
+  option2.series = series2;
+  console.log("series3", series3)
+  option3.series = series3;
+  console.log("series4", series4)
+  option4.series = series4;
+
+  if (option1 && typeof option1 === 'object') {
+    lineChart1.setOption(option1, true);
+  }
+  console.log("option1", option1)
+  if (option2 && typeof option2 === 'object') {
+    lineChart2.setOption(option2, true);
+  }
+  console.log("option2", option2)
+  if (option3 && typeof option3 === 'object') {
+    lineChart3.setOption(option3, true);
+  }
+  console.log("option3", option3)
+  if (option4 && typeof option4 === 'object') {
+    lineChart4.setOption(option4, true);
+  }
+  console.log("option4", option4)
+}
+
